778ce3dbb31bffd2313eb6b7cb560a6bb493f96a
carryma <kanma.cn@gmail.com>
2018-08-16 00:31:32 +0800

    String类的哈希值

---
+++ nowcoder/src/com/kanma/collections/hash/HashTest.java
@@ -0,0 +1,14 @@
+package com.kanma.collections.hash;
+
+/**
+ * @ Desc   ：测试hash函数(hash算法)
+ * @ Author ：MaKang
+ * @ Date   ：Created in 2018/8/15 23:49
+ */
+public class HashTest {
+    public static void main(String[] args) {
+        String str = "I am ok";
+        System.out.println(str.hashCode());
+    }
+
+}

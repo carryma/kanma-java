f750c3d6b2259fbbced5bdd1421c648ca6115a88
Kang Ma <kanma.cn@gmail.com>
2018-08-17 03:07:38 +0800

    并发和单线程执行测试

---
+++ nowcoder/src/com/kanma/ConcurrencyTest.java
@@ -0,0 +1,58 @@
+package com.kanma;
+
+/**
+ * @ Desc   ：并发和单线程执行测试
+ * @ Author ：MaKang
+ * @ Date   ：Created in 2018/8/17 3:04
+ */
+public class ConcurrencyTest {
+
+    /**
+     * 执行次数
+     */
+    private static final long count = 10000l;
+
+    public static void main(String[] args) throws InterruptedException {
+        //并发计算
+        concurrency();
+        //单线程计算
+        serial();
+    }
+
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+                System.out.println(a);
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        thread.join();
+        System.out.println("concurrency :" + time + "ms,b=" + b);
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial:" + time + "ms,b=" + b + ",a=" + a);
+    }
+
+}

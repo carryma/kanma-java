71a2f2c77099151667f02876c6a4fa779c282dc6
carryma <kanma.cn@gmail.com>
2018-08-20 01:40:49 +0800

    Transfer data between thread objects

--- nowcoder/src/com/kanma/concurrency/Thead1Test.java
+++ nowcoder/src/com/kanma/concurrency/Thead1Test.java
@@ -7,10 +7,12 @@ package com.kanma.concurrency;
  */
 public class Thead1Test {
     public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + "主线程运行开始!");
         Thread t1 = new Thread1("A");
         Thread t2 = new Thread1("B");
         t1.start();
         t2.start();
+        System.out.println(Thread.currentThread().getName() + "主线程运行结束!");
         /*
          * Java线程具有五中基本状态
          * (1)新建状态（New）：当线程对象对创建后，即进入了新建状态，如：Thread t = new MyThread();
@@ -21,10 +23,11 @@ public class Thead1Test {
          *                      注：就绪状态是进入到运行状态的唯一入口，也就是说，线程要想进入运行状态执行，首先必须处于就绪状态中；
          * (4)阻塞状态（Blocked）：处于运行状态中的线程由于某种原因，暂时放弃对CPU的使用权，停止执行，此时进入阻塞状态，
          *                      直到其进入到就绪状态，才 有机会再次被CPU调用以进入到运行状态。根据阻塞产生的原因不同，阻塞状态又可以分为三种：
-         *      1.等待阻塞：运行状态中的线程执行wait()方法，使本线程进入到等待阻塞状态；
-         *      2.同步阻塞 -- 线程在获取synchronized同步锁失败(因为锁被其它线程所占用)，它会进入同步阻塞状态；
-         *      3.其他阻塞 -- 通过调用线程的sleep()或join()或发出了I/O请求时，线程会进入到阻塞状态。
+         *      1.等待阻塞 -- 运行的线程执行wait()方法，JVM会把该线程放入等待池中(等待队列)。(wait会释放持有的锁)
+         *      2.同步阻塞 -- 运行的线程在获取对象的同步锁时，若该同步锁被别的线程占用，则JVM会把该线程放入锁池中。
+         *      3.其他阻塞 -- 运行的线程执行sleep()或join()方法，或者发出了I/O请求时，JVM会把该线程置为阻塞状态。
          *                    当sleep()状态超时、join()等待线程终止或者超时、或者I/O处理完毕时，线程重新转入就绪状态。
+         *                    （注意,sleep是不会释放持有的锁）
          * (5)死亡状态（Dead）：线程执行完了或者因异常退出了run()方法，该线程结束生命周期。
          */
     }
--- nowcoder/src/com/kanma/concurrency/Thread1.java
+++ nowcoder/src/com/kanma/concurrency/Thread1.java
@@ -10,19 +10,22 @@ public class Thread1 extends Thread {
     private String name;
 
     Thread1(String name) {
+        super(name);
         this.name = name;
     }
 
     @Override
     public void run() {
+        System.out.println(Thread.currentThread().getName() + " 线程运行开始!");
         for (int i = 1; i < 5; i++) {
-            System.out.println(name + " 线程运行" + i);
+            System.out.println("子线程" + name + "运行 : " + i);
             try {
                 sleep((int) Math.random() * 100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        System.out.println(Thread.currentThread().getName() + " 线程运行结束!");
     }
 
 }
